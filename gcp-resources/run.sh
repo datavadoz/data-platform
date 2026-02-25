4fcaf480bb65d3a41e0c99e7b286691ff96717fb
Danh Vo <danhvo.uit@gmail.com>
2026-02-25 04:44:03 +0000

    feat(gcp-resources): add log in run.sh

--- gcp-resources/run.sh
+++ gcp-resources/run.sh
@@ -8,7 +8,10 @@ terraform init
 echo "=== Terraform plan ==="
 terraform plan
 
+echo "=== Terraform apply ==="
 BRANCH_NAME=$(git branch --show-current)
 if [ "${BRANCH_NAME}" == "main" ]; then
   terraform apply -auto-approve
+else
+  echo "Ignore TF apply for ${BRANCH_NAME}"
 fi
