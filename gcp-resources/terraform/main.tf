77055ad8a4c0a646c829ec31bfc0db46f9328481
Danh Vo <danhvo.uit@gmail.com>
2026-02-28 08:00:13 +0000

    feat(gcp-resoureces): create SAs in CPS projects

--- gcp-resources/terraform/main.tf
+++ gcp-resources/terraform/main.tf
@@ -32,3 +32,27 @@ resource "google_project" "conda_cps_prod" {
   folder_id       = google_folder.cellphones.name
   deletion_policy = "DELETE"
 }
+
+module "sa_conda_cps_cloudrun_dev" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "4.7"
+  project_id    = google_project.conda_cps_dev.id
+  prefix        = "sa-"
+  names         = ["cloudrun"]
+  project_roles = [
+    "${google_project.conda_cps_dev.id}=>roles/bigquery.dataEditor",
+    "${google_project.conda_cps_dev.id}=>roles/bigquery.jobUser",
+  ]
+}
+
+module "sa_conda_cps_cloudrun_prod" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "4.7"
+  project_id    = google_project.conda_cps_prod.id
+  prefix        = "sa-"
+  names         = ["cloudrun"]
+  project_roles = [
+    "${google_project.conda_cps_prod.id}=>roles/bigquery.dataEditor",
+    "${google_project.conda_cps_prod.id}=>roles/bigquery.jobUser",
+  ]
+}
