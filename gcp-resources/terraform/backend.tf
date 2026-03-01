61f2c1e836df741c4ff4b1357c8047c1f8dee5d2
Danh Vo <danhvo.uit@gmail.com>
2026-03-01 14:02:54 +0000

    feat(gcp-resources): create 2 projects via module

---
+++ gcp-resources/terraform/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = " 7.21.0 "
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = " 7.21.0 "
+    }
+  }
+
+  backend "gcs" {
+    bucket = "conda-terraform"
+    prefix = "gcp-resources/state"
+  }
+}
---
+++ gcp-resources/terraform/cps.tf
@@ -0,0 +1,50 @@
+resource "google_folder" "cellphones" {
+  display_name = "CellphoneS"
+  parent       = "organizations/138569242965"
+}
+
+### DEV ###
+module "prj_conda_cps_dev" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "18.2"
+
+  name            = "conda-cps-dev"
+  billing_account = "01DDAB-3D0A6F-F91FAF"
+  deletion_policy = "DELETE"
+  folder_id       = google_folder.cellphones.name
+}
+
+module "sa_conda_cps_cloudrun_dev" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "4.7"
+  project_id    = google_project.conda_cps_dev.project_id
+  prefix        = "sa"
+  names         = ["cloudrun"]
+  project_roles = [
+    "${google_project.conda_cps_dev.project_id}=>roles/bigquery.dataEditor",
+    "${google_project.conda_cps_dev.project_id}=>roles/bigquery.jobUser",
+  ]
+}
+
+### PROD ###
+module "prj_conda_cps_prod" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "18.2"
+
+  name            = "conda-cps-prod"
+  billing_account = "01DDAB-3D0A6F-F91FAF"
+  deletion_policy = "DELETE"
+  folder_id       = google_folder.cellphones.name
+}
+
+module "sa_conda_cps_cloudrun_prod" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "4.7"
+  project_id    = google_project.conda_cps_prod.project_id
+  prefix        = "sa"
+  names         = ["cloudrun"]
+  project_roles = [
+    "${google_project.conda_cps_prod.project_id}=>roles/bigquery.dataEditor",
+    "${google_project.conda_cps_prod.project_id}=>roles/bigquery.jobUser",
+  ]
+}
