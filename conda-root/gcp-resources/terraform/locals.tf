be1a1793ec7b8614ac24720b45318cbacda3c501
Danh Vo <danhvo.uit@gmail.com>
2026-03-04 17:52:59 +0700

    feat(cps): remove unused locals

--- conda-root/gcp-resources/terraform/locals.tf
+++ conda-root/gcp-resources/terraform/locals.tf
@@ -1,16 +1,3 @@
 locals {
   cps_image = "${var.docker_registry}/${var.cps_image_name}:${var.cps_image_version}"
-
-  cps_activate_apis = [
-    "bigquery.googleapis.com",
-    "cloudbuild.googleapis.com",
-    "iam.googleapis.com",
-    "run.googleapis.com",
-  ]
-
-  cps_cloudrun_roles = [
-    "roles/bigquery.dataEditor",
-    "roles/bigquery.jobUser",
-    "roles/iam.serviceAccountTokenCreator",
-  ]
 }
