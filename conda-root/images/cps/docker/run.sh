59d51693d600100dc79149ee616fa3df0e193f1c
Danh Vo <danhvo.uit@gmail.com>
2026-03-05 22:22:12 +0700

    feat(cps): adapt run.sh to run on dev or main branch

--- conda-root/images/cps/docker/run.sh
+++ conda-root/images/cps/docker/run.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+set -e
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <dev|prod>"
+	exit 1
+fi
+
+if [ "$1" = "dev" ]; then
+	BRANCH="develop"
+elif [ "$1" = "prod" ]; then
+	BRANCH="main"
+else
+	echo "Invalid argument: $1"
+	echo "Usage: $0 <dev|prod>"
+	exit 1
+fi
+
 git clone https://github.com/datavadoz/data-platform.git
+git -C ./data-platform checkout "$BRANCH"
 python ./data-platform/conda-cps/main.py
