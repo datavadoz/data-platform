28ea304f926087d6cd9dfa95d152595092477984
Danh Vo <danhvo.uit@gmail.com>
2026-03-08 23:28:14 +0700

    fix(conda-cps): fix schema path

--- conda-cps/run-rate/main.py
+++ conda-cps/run-rate/main.py
@@ -16,7 +16,7 @@ bq.create_bq_table_from_gsheet_table(
     gsheet_table=GSheetTable(
         sheet_id='14zV1me4r6dHQn6c7nBbpW549eumP9OdfVfUq3kH51uQ',
         tab_name='Cost runrate!A2:AF71',
-        schema_name='run_rate.json'
+        schema_path='./schemas/run_rate.json'
     ),
     full_table_id=f"{project_id}.external_gsheet.run_rate",
     recreate_if_exists=True
