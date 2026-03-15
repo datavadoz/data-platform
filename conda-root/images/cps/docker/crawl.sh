0d98fadd69672f95ee5e55fdcf8d2a1b1ae15334
Danh Vo <danhvo.uit@gmail.com>
2026-03-15 20:17:30 +0700

    fix(conda-cps): add new arg to specify crawled platform

--- conda-root/images/cps/docker/crawl.sh
+++ conda-root/images/cps/docker/crawl.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 <dev|prod>"
+if [ "$#" -ne 2 ]; then
+	echo "Usage: $0 <dev|prod> <facebook|google>"
 	exit 1
 fi
 
@@ -13,11 +13,21 @@ elif [ "$1" = "prod" ]; then
 	BRANCH="main"
 else
 	echo "Invalid argument: $1"
-	echo "Usage: $0 <dev|prod>"
+	echo "Usage: $0 <dev|prod> <facebook|google>"
+	exit 1
+fi
+
+if [ "$2" = "facebook" ]; then
+	PLATFORM="facebook-ads"
+elif [ "$2" = "google" ]; then
+	PLATFORM="google-ads"
+else
+	echo "Invalid argument: $2"
+	echo "Usage: $0 <dev|prod> <facebook|google>"
 	exit 1
 fi
 
 git clone https://github.com/datavadoz/data-platform.git
 git -C ./data-platform checkout "$BRANCH"
 export PYTHONPATH=./data-platform/conda-cps:$PYTHONPATH
-python ./data-platform/conda-cps/facebook-ads/main.py --env "$1"
+python ./data-platform/conda-cps/"$PLATFORM"/main.py --env "$1"
