14aa2e49ff270258175a0877b857f9bf46e51b31
Danh Vo <danhvo.uit@gmail.com>
2026-04-12 12:05:37 +0700

    feat(cps): update Docker for traffic

---
+++ conda-root/images/cps/docker/traffic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <dev|prod>"
+	exit 1
+fi
+
+if [ "$1" = "dev" ]; then
+	BRANCH="develop"
+elif [ "$1" = "prod" ]; then
+	BRANCH="main"
+else
+	echo "Invalid argument: $1"
+	echo "Usage: $0 <dev|prod>"
+	exit 1
+fi
+
+git clone https://github.com/datavadoz/data-platform.git
+git -C ./data-platform checkout "$BRANCH"
+export PYTHONPATH=./data-platform/conda-cps:$PYTHONPATH
+python ./data-platform/conda-cps/traffic/main.py --env "$1"
